a5329d002d7ff16ca3755280e76a941557e29e35
Muhammad Rendra <mnrendra@users.noreply.github.com>
2024-06-17 20:42:40 +0800

    docs: update code comments

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,53 @@
 import type { TargetFunction, Options, StackTrace } from './types'
+
 import { createTarget } from './utils'
 
 /**
  * A utility that enables stack tracing of the `NodeJs.CallSite` object,
  * allowing dynamic tracing of invocations.
- * @param targetFunction An optional function that can serve as the target.
- * @param options An optional set of options to configure the output.
- * @returns An array containing objects of type `NodeJS.CallSite`.
+ *
+ * @param {Function} [targetFunction] - An optional function that can serve as
+ * the target.
+ * @param {Object} [options] - An optional set of options to configure the
+ * output.
+ *
+ * @returns {Array} An array containing objects of type `NodeJS.CallSite`.
+ *
  * @see https://github.com/mnrendra/stack-trace#readme
  */
 const main: StackTrace = function (
   targetFunction?: TargetFunction,
   {
     limit = 10
-  }: Options = {}
+  }: Options = {
+    limit: 10
+  }
 ) {
   // Initialize the `target` object.
   const target = createTarget()
 
-  // Destructure the necessary properties from the `Error` object.
+  // Destructure necessary properties from the `Error` object.
   const { stackTraceLimit, prepareStackTrace } = Error
 
-  // Manipulate the `Error.stackTraceLimit` based on the `limit` options.
+  // Set `Error.stackTraceLimit` based on the `limit` option.
   Error.stackTraceLimit = limit
 
-  // Manipulate the `Error.prepareStackTrace` to return only the `stack` object.
+  // Override `Error.prepareStackTrace` to return only the `stack` object.
   Error.prepareStackTrace = (_, stack) => stack
 
-  // Create the `.stack` property on the `target` object.
+  // Capture the stack trace and assign it to the `target` object.
   Error.captureStackTrace(target, targetFunction ?? main)
 
   // Destructure the `stack` property from the `target`.
   const { stack } = target
 
-  // Reset `Error.prepareStackTrace` to its original value.
+  // Restore `Error.prepareStackTrace` to its original value.
   Error.prepareStackTrace = prepareStackTrace
 
-  // Reset `Error.stackTraceLimit` to its original value.
+  // Restore `Error.stackTraceLimit` to its original value.
   Error.stackTraceLimit = stackTraceLimit
 
-  // Return the `stack` as the `NodeJs.CallSite` array.
+  // Return the `stack` as an array of `NodeJS.CallSite` objects.
   return stack
 }
 
--- src/utils/createTarget.ts
+++ src/utils/createTarget.ts
@@ -1,5 +1,10 @@
 import type { Target } from '../types'
 
+/**
+ * Create an initial stack target.
+ *
+ * @returns {Object} stack target.
+ */
 const createTarget = (): Target => ({
   stack: []
 })
