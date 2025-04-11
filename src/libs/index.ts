e6277177a02057cd4410b35467f555e032eacea7
Muhammad Rendra <mnrendra@users.noreply.github.com>
2025-04-11 16:16:57 +0700

    feat: trace fn names (#70)
    issues:
    * feat: trace fn names (#69)
    
    commits:
    * feat: trace fn names (1be6d79)

--- src/libs/index.ts
+++ src/libs/index.ts
@@ -1,7 +1,9 @@
 import traceFiles from './traceFiles'
+import traceFnNames from './traceFnNames'
 import traceStacks from './traceStacks'
 
 export {
   traceFiles,
+  traceFnNames,
   traceStacks
 }
--- src/libs/traceFnNames.ts
+++ src/libs/traceFnNames.ts
@@ -3,7 +3,7 @@ import type { Options } from '../types'
 import traceStacks from './traceStacks'
 
 /**
- * A utility for stack tracing the file names based on the `NodeJS.CallSite`
+ * A utility for stack tracing the function names based on the `NodeJS.CallSite`
  * object, enabling dynamic inspection of function calls.
  *
  * @param {((...args: any) => any)|null} [targetFn] - An optional function to
@@ -11,21 +11,21 @@ import traceStacks from './traceStacks'
  * @param {Options} [options] - An optional set of options to configure the
  * output.
  *
- * @returns {Array} A list of `NodeJS.CallSite` stack trace file names.
+ * @returns {Array} A list of `NodeJS.CallSite` stack trace function names.
  *
  * @see https://github.com/mnrendra/stack-trace#readme
  */
-const traceFiles = (
+const traceFnNames = (
   targetFn?: ((...args: any) => any) | null,
   {
     limit = 10
   }: Options = {}
 ): Array<string | null | undefined> => {
-  const stacks = traceStacks(targetFn ?? traceFiles, { limit })
+  const stacks = traceStacks(targetFn ?? traceFnNames, { limit })
 
-  const files = stacks.map((stack) => stack.getFileName())
+  const files = stacks.map((stack) => stack.getFunctionName())
 
   return files
 }
 
-export default traceFiles
+export default traceFnNames
